989dc4dab765ec25e0136d51f4a77f4396d6e7f2
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-04-15 10:37:27 +1000

    refactor Input component to handle file uploads

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -4,19 +4,25 @@ type InputProps = {
     label: string
     name: string
     placeholder: string
-    register: UseFormRegister<any>
+    ref?: React.Ref<HTMLInputElement>
+    register?: UseFormRegister<any>
+    handleFileChange?: any
+    field?: any
     className?: string
     children?: React.ReactNode
     type?: 'text' | 'number' | 'password' | 'file'
 }
-export const InputForm = ({label, name, placeholder,  type='text', register}: InputProps): JSX.Element => {
+export const InputForm = ({label, name, placeholder,  type='text', register, handleFileChange, ref, field}: InputProps): JSX.Element => {
     return (
     <div className="flex flex-auto mt-6 pt-4 w-full relative">
          <label className="absolute top-2 left-2 inline-block px-1 text-xs font-medium text-headline-text" htmlFor={name}>
             {label}
         </label>
         <div className="flex w-full mt-2 relative shadow-md">
-        <input className="w-full p-2 focus:ring-1 rounded sm:text-sm placeholder-body-text-light bg-bg-light" type={type} placeholder={placeholder} {...register(name)} />
+        <input className="w-full p-2 focus:ring-1 rounded sm:text-sm placeholder-body-text-light bg-bg-light" 
+        ref={ref} name={name} type={type} placeholder={placeholder}  {...(register && register(name))} 
+        onChange={handleFileChange && ((e)=> handleFileChange(e, field))}
+        />
         </div>
     </div>
     )
