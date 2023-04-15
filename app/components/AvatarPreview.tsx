bc05405b69ff529118124df4c648f74ad648f8ff
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-04-15 10:36:41 +1000

    Add avatarPreviewURL

--- app/components/AvatarPreview.tsx
+++ app/components/AvatarPreview.tsx
@@ -2,16 +2,16 @@
 import Image from 'next/image';
 
 type avatarProps = {
-    avatar?: string
+    avatarUrl?: string | null
     alt: string
 }
 
-export const AvatarPreview = ({avatar, alt}: avatarProps) => {
+export const AvatarPreview = ({avatarUrl, alt}: avatarProps) => {
 
     return (
         <div className=" bg-bg-light w-32 h-32 mx-auto mb-6 rounded-full flex items-center justify-center text-center text-body-text-light text-xl font-bold">
-            {avatar ? (
-              <Image src={avatar} alt={alt} className="w-full h-full object-cover rounded-full" />
+            {avatarUrl ? (
+              <Image src={avatarUrl} alt={alt} width={32} height={32} className="w-full h-full object-cover rounded-full" />
             ) : (
               'Upload Avatar'
             )}
