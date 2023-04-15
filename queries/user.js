5795ee88fb8e0ed0d9d8289e6892fae1a3e433fd
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-04-15 10:38:10 +1000

    refactor create user mutation

--- queries/user.js
+++ queries/user.js
@@ -4,14 +4,12 @@ export default {
   CREATE_USER: gql`
     mutation CreateUser($input: UserRegisterInput!) {
       createUser(input: $input) {
-        user {
           username
           email
           about
           avatar
           isActive
           role
-        }
       }
     }
   `,
