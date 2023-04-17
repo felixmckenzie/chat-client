2069a7b4ad2ce6df9c3b255cc674f71ad546d32d
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-04-17 13:29:02 +1000

    implement loader component

---
+++ app/components/Loader.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Loader() {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-highlight"></div>
+        </div>
+    )
+}
