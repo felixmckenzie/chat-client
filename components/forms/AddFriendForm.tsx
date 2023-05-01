6d8487b064f28a5b9906967a6f686c9aacddd9ad
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-05-01 17:37:20 +1000

    implement sendFriendRequest mutation, onSubmit function & error handling

--- components/forms/AddFriendForm.tsx
+++ components/forms/AddFriendForm.tsx
@@ -2,28 +2,58 @@
 import { FC } from 'react'
 import { useMutation } from '@apollo/client'
 import { getClient } from '@/lib/apollo'
-import { useForm, Controller } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-hot-toast'
 import { useUser, useAuth } from '@clerk/nextjs'
 import { Input } from '../Input'
 import queries from '@/queries'
+
+interface AddFriendInput {
+    contactUserEmail: string
+}
 
 export const AddFriendForm: FC = () => {
     const { user } = useUser()
     const { getToken } = useAuth()
     const client = getClient(getToken)
 
-    const { handleSubmit, register, control, formState, setValue } = useForm({
+    const { handleSubmit, register } = useForm<AddFriendInput>({
         defaultValues: {
-            email: '',
+            contactUserEmail: '',
         },
     })
 
+    const [sendFriendRequest, { error: friendRequestError }] = useMutation(queries.SEND_FRIEND_REQUEST, {
+        client,
+        onCompleted: (data) => {
+            const receiverUsername = data?.sendFriendRequest?.receiver.username
+            toast.success(`Friend Request Sent to ${receiverUsername}`)
+        },
+        onError: (e) => {
+            if (e && friendRequestError) {
+                toast.error(friendRequestError?.message)
+            } else {
+                toast.error('Failed to Send! Please Try Again Later')
+            }
+        },
+    })
+
+    const onSubmit = async (data: AddFriendInput) => {
+        try {
+            await sendFriendRequest({ variables: { clerkId: user?.id, contactUserEmail: data.contactUserEmail } })
+        } catch (e) {
+            toast.error(e.message)
+        }
+    }
+
     return (
-        <div className="mx-auto">
-            <form>
-                <Input label="CaSe SeNsItIvE" name="addFriend" placeholder="Enter an Email Address" register={register}>
+        <div className="w-1/2">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input label="CaSe SeNsItIvE" name="addFriend" placeholder="Enter Email Address" register={register}>
                     <div className="absolute inset-y-0 right-0 flex items-center px-2">
-                        <button className=" bg-tertiary rounded p-2 text-headline-text text-xs">Send Friend Request</button>
+                        <button type="submit" className=" bg-tertiary rounded p-2 text-headline-text text-xs">
+                            Send Friend Request
+                        </button>
                     </div>
                 </Input>
             </form>
